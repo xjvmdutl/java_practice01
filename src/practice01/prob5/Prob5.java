677fa4d76b57088c91e51868750106caafc6a07f
BIT <BIT@DESKTOP-9CMSKCR>
2019-08-06 16:40:57 +0900

    5번

--- src/practice01/prob5/Prob5.java
+++ src/practice01/prob5/Prob5.java
@@ -1,6 +1,16 @@
 package practice01.prob5;
-
 public class Prob5 {
 	public static void main (String args[]) {
+		for(int i=1;i<=99;++i) {
+			if(i<=10) {
+				if(i%3==0)
+					System.out.println(i+"¦");
+			}else {
+				if((i/10)%3==0)
+						System.out.println(i+"¦");
+				else if(i%10!=0 && (i%10)%3==0)
+						System.out.println(i+"¦");
+			}
+		}
 	}
 }
