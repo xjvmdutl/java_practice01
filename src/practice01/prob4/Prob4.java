168fc985317ac1227980bfafb8dedaf83c0bb395
BIT <BIT@DESKTOP-9CMSKCR>
2019-08-06 16:14:46 +0900

    4번 완료

--- src/practice01/prob4/Prob4.java
+++ src/practice01/prob4/Prob4.java
@@ -1,6 +1,15 @@
 package practice01.prob4;
-
+import java.util.Scanner;
 public class Prob4 {
 	public static void main (String args[]) {
+		Scanner sc =new Scanner(System.in);
+		System.out.print("문자열을 입력하세요 : ");
+		String s= sc.nextLine();
+		for(int i =0;i<s.length();++i) {
+			for(int j=0;j<=i;++j) {
+				System.out.print(s.charAt(j));
+			}
+			System.out.println();
+		}
 	}
 }
